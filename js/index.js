b7d15b25df4871254f42e36b058f2b1fd8812d6e
Jonas Lazar <jonaslazarvda@gmail.com>
2023-06-09 14:18:59 +0200

    aaa

---
+++ js/index.js
@@ -0,0 +1,22 @@
+var lastScroll = 0;
+
+var customScroll = (e) => {
+    e.preventDefault();
+    st = window.pageYOffset || document.documentElement.scrollTop;
+    if (lastScroll%document.documentElement.clientHeight != 0 && lastScroll != document.documentElement.scrollHeight-document.documentElement.clientHeight) {
+        lastScroll = st <= 0? 0 : st;
+        return
+    }
+    let page = Math.floor(st/document.documentElement.clientHeight)+(st > lastScroll? 1 : 0);
+    window.scrollTo({top: page*document.documentElement.clientHeight, behavior: "smooth",})
+    lastScroll = st <= 0? 0 : st;
+    let navbar = document.querySelector(".navbar")
+    if (page == 0)
+        navbar.classList.add("navtop");
+    else
+        navbar.classList.remove("navtop");
+}
+
+document.addEventListener("scroll", customScroll, false);
+document.addEventListener("mousewheel", customScroll, false);
+document.addEventListener("touchmove", customScroll, false);
---
+++ js/navbar.js
@@ -0,0 +1,4 @@
+function toggleNavLinks() {
+    links = document.querySelector(".navlinks");
+    links.classList.contains("hidden") ? links.classList.remove("hidden") : links.classList.add("hidden")
+}
